c268804fad7f2b5ae0cdd69a321eae3b87473db0
Prayas Gupta <prayasgupta670@gmail.com>
2026-09-02 23:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (55.21%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,20 @@
+class Solution:
+    def plusOne(self, digits):
+        
+        i = len(digits) - 1
+
+        while i >= 0:
+
+            if digits[i] < 9:
+                digits[i] = digits[i] + 1
+                return digits
+        
+            digits[i] = 0
+
+            i = i - 1
+
+        return[1] + digits        
+
+        
+        
+        
