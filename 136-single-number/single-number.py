c348729af1a713a6bb713ce5e750fda3449156aa
Prayas Gupta <prayasgupta670@gmail.com>
2026-09-03 22:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (79.91%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        result = 0
+
+        for num in nums:
+            result = num ^ result
+
+        return result    
+        
+        
